9ff1c69ba9fbb001cb52f0d585371733ae582508
alok <sharmaalok0785@gmail.com>
2024-10-01 22:23:03 +0530

    Configure and Commit

---
+++ Array/arrayaintro.js
@@ -0,0 +1,19 @@
+let a  = [1,2,3,4,"aniket",false];
+console.log(a);
+
+// let b = new Array(); // it will create empty array
+let b = new Array("abc", "def", "ghi");
+console.log(b);
+
+/**
+ *
+ * given array - > [11,21,31,41,51]
+ * indexes/position - > [0, 1, 2, 3, 4] // last element index = arr.length-1
+ * 
+ * */
+
+console.log(a[0]) //- > 1
+
+a[4] = 5;
+console.log(a); //- > [ 1, 2, 3, 4, 5, false ]
+
