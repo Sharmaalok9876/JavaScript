d2275e6a12e6a5d04fbe0d1c11adf5615da7ae95
alok <sharmaalok0785@gmail.com>
2024-10-01 22:43:08 +0530

    Configure and Commit

---
+++ Array/function.js
@@ -0,0 +1,10 @@
+function greet() {
+    console.log("Hi there");
+    console.log("how are you");
+}
+
+function sqr(x) { // x-> parameter
+    console.log(x*x);
+}
+greet();
+sqr(5); // 5-> argument
